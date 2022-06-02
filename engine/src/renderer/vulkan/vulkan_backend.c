b4ece2f5a82e6e96e0e59f3d199aa48c59031f33
Chris Eckhardt <ceckhardt06@gmail.com>
2022-06-01 21:08:56 -0400

    vulkan validation, extensions, and debug messaging

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -1,19 +1,23 @@
 #include "vulkan_backend.h"
 
 #include "vulkan_types.inl"
+#include "vulkan_platform.h"
 
 #include "core/logger.h"
 #include "core/kstring.h"
 
 #include "containers/darray.h"
 
-#include "platform/platform.h"
-
 
 
 static vulkan_context context;
 
 
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+    VkDebugUtilsMessageTypeFlagsEXT message_types,
+    const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+    void* user_data);
 
 
 b8 vulkan_renderer_backend_initialize(renderer_backend* backend, const char* application_name, struct platform_state* plat_state) {
@@ -48,16 +52,66 @@ b8 vulkan_renderer_backend_initialize(renderer_backend* backend, const char* app
     create_info.enabledExtensionCount = darray_length(required_extensions);
     create_info.ppEnabledExtensionNames = required_extensions;
 
+    const char** required_validation_layer_names = 0;
+    u32 required_validation_layer_count = 0;
 
+#if defined(_DEBUG)
+    KINFO("Validation layers enabled. Enumerating...");
 
-    create_info.enabledLayerCount = 0;
-    create_info.ppEnabledLayerNames = 0;
+    required_validation_layer_names = darray_create(const char*);
+    darray_push(required_validation_layer_names, &"VK_LAYER_KHRONOS_validation");
+    required_validation_layer_count = darray_length(required_validation_layer_names);
 
-    VkResult result = vkCreateInstance(&create_info, context.allocator, &context.instance);
-    if (result != VK_SUCCESS) {
-        KERROR("vkCreateInstance failed with result: %u", result);
-        return false;
+    u32 available_layer_count = 0;
+    VK_CHECK(vkEnumerateInstanceLayerProperties(&available_layer_count, 0));
+    VkLayerProperties* available_layers = darray_reserve(VkLayerProperties, available_layer_count);
+    VK_CHECK(vkEnumerateInstanceLayerProperties(&available_layer_count, available_layers));
+
+    for (u32 i = 0; i < required_validation_layer_count; ++i) {
+        KINFO("Searching for layer: %s...", required_validation_layer_names[i]);
+        b8 found = false;
+        for (u32 j = 0; j < available_layer_count; ++j) {
+            if (strings_equal(required_validation_layer_names[i], available_layers[j].layerName)) {
+                found = true;
+                KINFO("Found.");
+                break;
+            }
+        }
+
+        if (!found) {
+            KFATAL("Required validation layer is missing: %s", required_validation_layer_names[i]);
+            return false;
+        }
     }
+    KINFO("All required validation layers are present.");
+#endif
+
+    create_info.enabledLayerCount = required_validation_layer_count;
+    create_info.ppEnabledLayerNames = required_validation_layer_names;
+
+    VK_CHECK(vkCreateInstance(&create_info, context.allocator, &context.instance));
+    KINFO("Vulkan instance created.");
+
+#if defined(_DEBUG)
+    KDEBUG("Creating Vulkan debugger...");
+
+    u32 log_severity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+                       VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT; // its possible to add info and verbose levels here
+
+    VkDebugUtilsMessengerCreateInfoEXT debug_create_info = {VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT};
+    debug_create_info.messageSeverity = log_severity;
+    debug_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | 
+                                    VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |
+                                    VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+    debug_create_info.pfnUserCallback = vk_debug_callback;
+    debug_create_info.pUserData = 0;
+
+    PFN_vkCreateDebugUtilsMessengerEXT func = 
+        (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(context.instance, "vkCreateDebugUtilsMessengerEXT");
+    KASSERT_MSG(func, "Failed to create Vulkan debug messenger.");
+    VK_CHECK(func(context.instance, &debug_create_info, context.allocator, &context.debug_messenger));
+    KDEBUG("Vulkan debugger created.");
+#endif
 
     KINFO("vulkan renderer initialized successfully.");
     return true;
@@ -65,6 +119,15 @@ b8 vulkan_renderer_backend_initialize(renderer_backend* backend, const char* app
 
 void vulkan_renderer_backend_shutdown(renderer_backend* backend) {
 
+    KDEBUG("Destroying Vulkan Debugger...");
+    if (context.debug_messenger) {
+        PFN_vkDestroyDebugUtilsMessengerEXT func =
+            (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(context.instance, "vkDestroyDebugUtilsMessengerEXT");
+        func(context.instance, context.debug_messenger, context.allocator);
+    }
+
+    KDEBUG("Destroying Vulkan instance...");
+    vkDestroyInstance(context.instance, context.allocator);
 }
 
 void vulkan_renderer_backend_on_resized(renderer_backend* backend, u16 width, u16 height) {
@@ -77,4 +140,28 @@ b8 vulkan_renderer_backend_begin_frame(renderer_backend* backend, f32 delta_time
 
 b8 vulkan_renderer_backend_end_frame(renderer_backend* backend, f32 delta_time) {
     return true;
+}
+
+
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+    VkDebugUtilsMessageTypeFlagsEXT message_types,
+    const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+    void* user_data) {
+    switch (message_severity) {
+        default:
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            KERROR(callback_data->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            KWARN(callback_data->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+            KINFO(callback_data->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+            KTRACE(callback_data->pMessage);
+            break;
+    }
+    return VK_FALSE;
 }
